00252bac4507d1a90da3e4be6f920f60469d32ed
Andrew <62994051+Rarmash@users.noreply.github.com>
2024-02-22 00:39:55 +0300

    Added GameRating model.

--- b4cklog/models.py
+++ b4cklog/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 from PIL import Image
 from django.db.models.signals import pre_save
 from django.dispatch import receiver
@@ -10,7 +11,7 @@ class Platform(models.Model):
     name = models.CharField(max_length=100)
     abbreviation = models.CharField(max_length=100, blank=True)
 
-    def __str__(self):  # Исправлена ошибка в методе str
+    def __str__(self):
         return self.name
 
 
@@ -18,9 +19,30 @@ class Game(models.Model):
     igdb_id = models.IntegerField(unique=True)
     name = models.CharField(max_length=200)
     summary = models.TextField()
-    cover = models.URLField(blank=True)  # Убрали default значение
+    cover = models.URLField(blank=True)
     first_release_date = models.DateField(null=True)
     platforms = models.ManyToManyField(Platform)
+    ratings = models.ManyToManyField(User, through='GameRating', related_name='rated_games')
 
-    def __str__(self):  # Исправлена ошибка в методе str
+    @property
+    def average_rating(self):
+        ratings = self.ratings.all()
+        if ratings:
+            return sum([rating.rating for rating in ratings]) / len(ratings)
+        else:
+            return None
+
+    def __str__(self):
         return self.name
+
+
+class GameRating(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 11)])
+
+    class Meta:
+        unique_together = ('user', 'game')
+
+    def __str__(self):
+        return f'{self.user.username}\'s rating for {self.game.name}: {self.rating}'
