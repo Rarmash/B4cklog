1359226581cca611692fd19f468765e314a3278c
Andrew <62994051+Rarmash@users.noreply.github.com>
2024-04-04 19:58:09 +0300

    Added "README.md" and some comment strings.

--- b4cklog/models.py
+++ b4cklog/models.py
@@ -1,48 +1,48 @@
 from django.db import models
 from django.contrib.auth.models import User
-from PIL import Image
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-from django.contrib.postgres.fields import ArrayField
 
 
+# Модель платформы, на которой доступны игры.
 class Platform(models.Model):
-    platform_id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=100)
-    abbreviation = models.CharField(max_length=100, blank=True)
+    platform_id = models.IntegerField(unique=True)  # Уникальный идентификатор платформы.
+    name = models.CharField(max_length=100)  # Название платформы
+    abbreviation = models.CharField(max_length=100, blank=True)  # Аббревиатура платформы (необязательно).
 
     def __str__(self):
-        return self.name
+        return self.name  # Возвращает строковое представление объекта - название платформы.
 
 
+# Модель игры.
 class Game(models.Model):
-    igdb_id = models.IntegerField(unique=True)
-    name = models.CharField(max_length=200)
-    summary = models.TextField()
-    cover = models.URLField(blank=True)
-    first_release_date = models.DateField(null=True)
-    platforms = models.ManyToManyField(Platform)
-    ratings = models.ManyToManyField(User, through='GameRating', related_name='rated_games')
+    igdb_id = models.IntegerField(unique=True)  # Уникальный идентификатор игры в базе данных IGDB.
+    name = models.CharField(max_length=200)  # Краткое описание игры.
+    summary = models.TextField()  # Ссылка на обложку игры (необязательно).
+    cover = models.URLField(blank=True)  # Дата первого релиза игры (может быть пустой).
+    first_release_date = models.DateField(null=True)  # Дата первого релиза игры (может быть пустой).
+    platforms = models.ManyToManyField(Platform)  # Связь многие-ко-многим с моделью Platform.
+    ratings = models.ManyToManyField(User, through='GameRating',
+                                     related_name='rated_games')  # Оценки пользователей для игры.
 
     @property
     def average_rating(self):
-        ratings = self.ratings.all()
-        if ratings:
-            return sum([rating.rating for rating in ratings]) / len(ratings)
+        ratings = self.ratings.all()  # Получение всех оценок для данной игры.
+        if ratings:  # Если есть оценки.
+            return sum([rating.rating for rating in ratings]) / len(ratings)  # Возвращаем среднюю оценку.
         else:
             return None
 
     def __str__(self):
-        return self.name
+        return self.name  # Возвращает строковое представление объекта - название игры.
 
 
 class GameRating(models.Model):
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
-    game = models.ForeignKey(Game, on_delete=models.CASCADE)
-    rating = models.IntegerField(choices=[(i, i) for i in range(1, 11)])
+    user = models.ForeignKey(User, on_delete=models.CASCADE)  # Пользователь, оценивший игру.
+    game = models.ForeignKey(Game, on_delete=models.CASCADE)  # Игра, которую пользователь оценил.
+    rating = models.IntegerField(choices=[(i, i) for i in range(1, 11)])  # Оценка игры от 1 до 10.
 
     class Meta:
-        unique_together = ('user', 'game')
+        unique_together = ('user', 'game')  # Уникальность комбинации пользователя и игры.
 
     def __str__(self):
+        # Возвращает строковое представление объекта - оценка игры пользователем.
         return f'{self.user.username}\'s rating for {self.game.name}: {self.rating}'
